a4ef161a5604ed9c775e4091a360032aa77c6b0a
james.griba-at-101749551814 <james.griba@myxlog.com>
2020-06-26 19:25:12 +0800

    Created AuditLog model to handle create request

---
+++ xgca.core/Models/AuditLog/CreatedAuditLog.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace xgca.core.Models.AuditLog
+{
+    public class CreatedAuditLog
+    {
+        public string AuditLogAction { get; set; }
+        public string TableName { get; set; }
+        public int KeyFieldId { get; set; }
+        public string? OldValue { get; set; }
+        public string NewValue { get; set; }
+        public int CreatedBy { get; set; }
+        public string CreatedByName { get; set; }
+        public DateTime CreatedOn { get; set; }
+        public Guid Guid { get; set; }
+    }
+}
