886f1cbf7b58ca2ac2476dcadadc6089bb8b7cd5
james.griba-at-101749551814 <james.griba@myxlog.com>
2020-07-13 00:23:08 +0800

    Added migration for adding column on audit log entity

---
+++ xgca.entity/Migrations/20200712161427_added_column_changed_datatype.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace xgca.entity.Migrations
+{
+    public partial class added_column_changed_datatype : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "CreatedByName",
+                schema: "Settings",
+                table: "AuditLog",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "CreatedByName",
+                schema: "Settings",
+                table: "AuditLog",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldNullable: true);
+        }
+    }
+}
