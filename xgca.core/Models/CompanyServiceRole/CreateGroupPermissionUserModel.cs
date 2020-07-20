83956b30085fe38ef572ec848dbc3ee7ed72c894
Jerson Baltazar <work.jerson.baltazar@gmail.com>
2020-07-20 18:33:52 +0800

    Group Update -> Done update of group with user WIP on permission (create/update)

--- xgca.core/Models/CompanyServiceRole/CreateGroupPermissionUserModel.cs
+++ xgca.core/Models/CompanyServiceRole/CreateGroupPermissionUserModel.cs
@@ -5,14 +5,15 @@ using System.Text;
 
 namespace xgca.core.Models.CompanyServiceRole
 {
+    // -- BASE CLASS (GROUP, PERMISSION, USERS)
     public class CreateGroupPermissionUserModel : CreateCompanyServiceRoleModel
     {
         [Required]
         public string CompanyServiceGuid { get; set; }
-
+        public ICollection<ClientMenuModel> Permissions { get; set; }
         public ICollection<CreateNewUserPerGroupModuleModel> CompanyServiceUsersArray { get; set; }
     }
-
+    // -- USER
     public class CreateNewUserPerGroupModuleModel
     {
         public int CompanyServiceId { get; set; }
@@ -22,5 +23,53 @@ namespace xgca.core.Models.CompanyServiceRole
         [Required]
         public int CompanyId { get; set; }
         public int CompanyServiceRoleId { get; set; }
+    }
+    // -- PERMISSIONS
+    public class ClientMenuModel
+    {
+        public string Display { get; set; }
+        public string Description { get; set; }
+        public string Guid { get; set; }
+        public string Index { get; set; }
+        public ICollection<ClientSubMenuModel> ClientSubMenus { get; set; }
+        public ICollection<ClientMenuModuleModel> ClientMenuModules { get; set; }
+    }
+    public class ClientSubMenuModel
+    {
+        public string Display { get; set; }
+        public string Description { get; set; }
+        public string Guid { get; set; }
+        public string Index { get; set; }
+        public ICollection<ClientMenuModuleModel> ClientMenuModules { get; set; }
+    }
+    public class ClientMenuModuleModel
+    {
+        public string Guid { get; set; }
+        public int ClientMenuId { get; set; }
+        public int ClientSubMenuId { get; set; }
+        public int ModuleId { get; set; }
+        public ModuleModel Module { get; set; }
+    }
+    public class ModuleModel
+    {
+        public int ModuleId { get; set; }
+        public string ModuleName { get; set; }
+        public string Description { get; set; }
+        public int ResourceTypeId { get; set; }
+        public ICollection<ModuleGroupModel> ModuleGroups { get; set; }
+    }
+    public class ModuleGroupModel
+    {
+        public int ModuleGroupId { get; set; }
+        public int ModuleId { get; set; }
+        public int ResourceGroupId { get; set; }
+        public ResourceGroupModel ResourceGroups { get; set; }
+    }
+    public class ResourceGroupModel
+    {
+        public int ResourceGroupId { get; set; }
+        public string ResourceGroupName { get; set; }
+        public string Description { get; set; }
+        public string Guid { get; set; }
     }
 }
