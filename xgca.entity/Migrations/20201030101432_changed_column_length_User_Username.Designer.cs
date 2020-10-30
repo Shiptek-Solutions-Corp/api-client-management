31bd0c2387db9ea9fe71b54696a08bb2a6a43a7c
James Griba <james.griba@myxlog.com>
2020-10-30 18:24:52 +0800

    Added migration for adjusting username max characters

--- xgca.entity/Migrations/20201030101432_changed_column_length_User_Username.Designer.cs
+++ xgca.entity/Migrations/20201030101432_changed_column_length_User_Username.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using xgca.entity;
 
 namespace xgca.entity.Migrations
 {
     [DbContext(typeof(XGCAContext))]
-    partial class XGCAContextModelSnapshot : ModelSnapshot
+    [Migration("20201030101432_changed_column_length_User_Username")]
+    partial class changed_column_length_User_Username
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ xgca.entity/Migrations/20201030101432_changed_column_length_User_Username.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace xgca.entity.Migrations
+{
+    public partial class changed_column_length_User_Username : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Username",
+                schema: "Users",
+                table: "User",
+                maxLength: 24,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(20)",
+                oldMaxLength: 20,
+                oldNullable: true);
+
+            migrationBuilder.UpdateData(
+                schema: "Settings",
+                table: "AddressType",
+                keyColumn: "AddressTypeId",
+                keyValue: 1,
+                columns: new[] { "CreatedOn", "Guid", "ModifiedOn" },
+                values: new object[] { new DateTime(2020, 10, 30, 10, 14, 31, 840, DateTimeKind.Utc).AddTicks(7086), new Guid("1e0621b2-b7ea-4d48-8be2-f09980694816"), new DateTime(2020, 10, 30, 10, 14, 31, 840, DateTimeKind.Utc).AddTicks(8830) });
+
+            migrationBuilder.UpdateData(
+                schema: "Settings",
+                table: "AddressType",
+                keyColumn: "AddressTypeId",
+                keyValue: 2,
+                columns: new[] { "CreatedOn", "Guid", "ModifiedOn" },
+                values: new object[] { new DateTime(2020, 10, 30, 10, 14, 31, 844, DateTimeKind.Utc).AddTicks(714), new Guid("95ec682b-074f-42bb-9fed-d4dbde41e41d"), new DateTime(2020, 10, 30, 10, 14, 31, 844, DateTimeKind.Utc).AddTicks(755) });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Username",
+                schema: "Users",
+                table: "User",
+                type: "nvarchar(20)",
+                maxLength: 20,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldMaxLength: 24,
+                oldNullable: true);
+
+            migrationBuilder.UpdateData(
+                schema: "Settings",
+                table: "AddressType",
+                keyColumn: "AddressTypeId",
+                keyValue: 1,
+                columns: new[] { "CreatedOn", "Guid", "ModifiedOn" },
+                values: new object[] { new DateTime(2020, 10, 27, 6, 37, 46, 96, DateTimeKind.Utc).AddTicks(5368), new Guid("1e0621b2-b7ea-4d48-8be2-f09980694816"), new DateTime(2020, 10, 27, 6, 37, 46, 96, DateTimeKind.Utc).AddTicks(7262) });
+
+            migrationBuilder.UpdateData(
+                schema: "Settings",
+                table: "AddressType",
+                keyColumn: "AddressTypeId",
+                keyValue: 2,
+                columns: new[] { "CreatedOn", "Guid", "ModifiedOn" },
+                values: new object[] { new DateTime(2020, 10, 27, 6, 37, 46, 99, DateTimeKind.Utc).AddTicks(3869), new Guid("95ec682b-074f-42bb-9fed-d4dbde41e41d"), new DateTime(2020, 10, 27, 6, 37, 46, 99, DateTimeKind.Utc).AddTicks(3921) });
+        }
+    }
+}
