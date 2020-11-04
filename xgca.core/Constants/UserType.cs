eba5fcea2fd41fdbbce0060c8ed8d31d3e8c35cc
James Griba <james.griba@myxlog.com>
2020-11-04 20:59:08 +0800

    Created UserType enum for Master and Normal User global value

---
+++ xgca.core/Constants/UserType.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace xgca.core.Constants
+{
+    public enum UserType
+    {
+        None,           // 0
+        MasterUser,     // 1
+        NormalUser      // 2
+    }
+}
