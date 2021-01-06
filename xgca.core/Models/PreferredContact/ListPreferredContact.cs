dcd4891b6b3513f037ec0a13d2152716f02554ae
James Griba <james.griba@myxlog.com>
2021-01-06 10:32:17 +0800

    Added model for listing complete contact information

--- xgca.core/Models/PreferredContact/ListPreferredContact.cs
+++ xgca.core/Models/PreferredContact/ListPreferredContact.cs
@@ -4,7 +4,7 @@ using System.Text;
 
 namespace xgca.core.Models.PreferredContact
 {
-    public class ViewPreferredContact
+    public class ListPreferredContact
     {
         public string PreferredContactId { get; set; }
         public string ContactId { get; set; }
@@ -13,5 +13,8 @@ namespace xgca.core.Models.PreferredContact
         public string ImageURL { get; set; }
         public string CityProvince { get; set; }
         public string Country { get; set; }
+        public string PhoneNumber { get; set; }
+        public string MobileNumber { get; set; }
+        public string FaxNumber { get; set; }
     }
 }
