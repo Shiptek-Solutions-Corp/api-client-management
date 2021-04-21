1d6c5b7af170e730cfdd8db209529aa6df09e988
James Griba <james.griba@myxlog.com>
2021-04-21 10:33:11 +0800

    Changed endpoint to user/{username}/details

--- xlog_client_management_api/Controllers/User/UserController.cs
+++ xlog_client_management_api/Controllers/User/UserController.cs
@@ -116,7 +116,7 @@ namespace xlog_client_management_api.Controllers.User
             return Ok(response);
         }
 
-        [Route("user/{username}")]
+        [Route("user/{username}/details")]
         [HttpGet]
         [Authorize(AuthenticationSchemes = "Bearer")]
         //[TokenAuthorize("scope", "users.get")]
