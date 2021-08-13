83bdc066b12afd5c0a518731dd8071389af3269c
rtsantos-github <roldantsantos@gmail.com>
2021-08-13 09:28:07 +0800

    Setup Accreditation

--- xlog_client_management_api/Controllers/Accreditation/AccreditationController.cs
+++ xlog_client_management_api/Controllers/Accreditation/AccreditationController.cs
@@ -15,40 +15,10 @@ namespace xlog_client_management_api.Controllers.Accreditation
     [ApiController]
     public class AccreditationController : ControllerBase
     {
-        private static readonly string[] Summaries = new[]
-       {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-        };
-
-        private readonly ILogger<WeatherForecastController> _logger;
-        private readonly IAuthorizationService _authorizationService;
-
-        public AccreditationController(
-            ILogger<WeatherForecastController> logger,
-            IAuthorizationService authorizationService
-            )
+        
+        public AccreditationController()
         {
-            _logger = logger;
-            _authorizationService = authorizationService;
-        }
-        [HttpGet]
-        [Route("time-info")]
-        public IActionResult GetTimeInfo()
-        {
-            return Ok(TimeZoneInfo.GetSystemTimeZones());
-        }
 
-        [HttpGet]
-        public IEnumerable<WeatherForecast> Get()
-        {
-            var rng = new Random();
-            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
-            {
-                Date = DateTime.Now.AddDays(index),
-                TemperatureC = rng.Next(-20, 55),
-                Summary = Summaries[rng.Next(Summaries.Length)]
-            })
-            .ToArray();
         }
     }
 }
