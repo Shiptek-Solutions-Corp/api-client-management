de1c707f84bea1f664a660a55eaceefe6d209971
rtsantos-github <roldantsantos@gmail.com>
2021-08-16 09:01:14 +0800

    Add Partial DBContext

--- xgca.entity/XGCAContext.cs
+++ xgca.entity/XGCAContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,42 +10,7 @@ using static xgca.entity.Models._Model;
 
 namespace xgca.entity
 {
-    public interface IXGCAContext
-    {
-        Task<int> SaveChangesAsync();
-        Task<int> SaveChangesAuditable();
-        DbSet<User> Users { get; set; }
-
-        DbSet<Address> Addresses { get; set; }
-        DbSet<AddressType> AddressTypes { get; set; }
-        DbSet<AuditLog> AuditLogs { get; set; }
-        DbSet<Company> Companies { get; set; }
-        DbSet<ContactDetail> ContactDetails { get; set; }
-        DbSet<CompanyService> CompanyServices { get; set; }
-        DbSet<CompanyServiceRole> CompanyServiceRoles { get; set; }
-        DbSet<CompanyServiceUser> CompanyServiceUsers { get; set; }
-        DbSet<CompanyUser> CompanyUsers { get; set; }
-        DbSet<CompanyGroupResource> CompanyGroupResources { get; set; }
-        DbSet<CompanyServiceUserRole> CompanyServiceUserRoles { get; set; }
-        DbSet<Guest> Guests { get; set; }
-        DbSet<PreferredContact> PreferredContacts { get; set; }
-        DbSet<PreferredProvider> PreferredProviders { get; set; }
-        DbSet<Invite> Invites { get; set; }
-        DbSet<BeneficialOwnersType> BeneficialOwnersTypes { get; set; }
-        DbSet<CompanyBeneficialOwners> CompanyBeneficialOwners { get; set; }
-        DbSet<CompanyDirectors> CompanyDirectors { get; set; }
-        DbSet<CompanyDocuments> CompanyDocuments { get; set; }
-        DbSet<CompanySections> CompanySections { get; set; }
-        DbSet<CompanyStructure> CompanyStructures { get; set; }
-        DbSet<DocumentCategory> DocumentCategories { get; set; }
-        DbSet<DocumentType> DocumentTypes { get; set; }
-        DbSet<KycStatus> KycStatuses { get; set; }
-        DbSet<Section> Sections { get; set; }
-        DbSet<SectionStatus> SectionStatuses { get; set; }
-        DbSet<CompanyTaxSettings> CompanyTaxSettings { get; set; }
-        DbSet<KYCLog> KYCLogs { get; set; }
-
-    }
+    
     public static class ModelBuilderExtensions
     {
         public static void SeedDefaultValues(this ModelBuilder modelBuilder)
@@ -74,14 +40,15 @@ namespace xgca.entity
                 });
         }
     }
-    public class XGCAContext : DbContext, IXGCAContext
+    public partial class XGCAContext : DbContext
     {
-        public XGCAContext(DbContextOptions options)
-            : base(options)
-        {
-        }
-        public DbSet<User> Users { get; set; }
+        private readonly IHttpContextAccessor contextAccessor;
 
+        public XGCAContext(DbContextOptions<XGCAContext> options, IHttpContextAccessor contextAccessor)
+         : base(options)
+        { this.contextAccessor = contextAccessor; }
+
+        public DbSet<User> Users { get; set; }
         public DbSet<Address> Addresses { get; set; }
         public DbSet<AddressType> AddressTypes { get; set; }
         public DbSet<AuditLog> AuditLogs { get; set; }
@@ -115,12 +82,7 @@ namespace xgca.entity
         public DbSet<CompanyTaxSettings> CompanyTaxSettings { get; set; }
         public DbSet<KYCLog> KYCLogs { get; set; }
 
-        public Task<int> SaveChangesAsync() => base.SaveChangesAsync();
-        public Task<int> SaveChangesAuditable()
-        {
-            UpdateAuditEntities();
-            return base.SaveChangesAsync();
-        }
+      
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             modelBuilder.Entity<CompanyTaxSettings>(entity => {
@@ -699,44 +661,6 @@ namespace xgca.entity
 
             base.OnModelCreating(modelBuilder);
         }
-        private void UpdateAuditEntities()
-        {
-            // https://medium.com/@unhandlederror/deleting-it-softly-with-ef-core-5f191db5cf72
-            var modifiedEntries = ChangeTracker.Entries()
-                .Where(x => (x.Entity is ISoftDeletableEntity || x.Entity is IAuditableEntity) && (x.State == EntityState.Added || x.State == EntityState.Modified || x.State == EntityState.Deleted));
-
-
-            foreach (var entry in modifiedEntries)
-            {
-                var entity = (IAuditableEntity)entry.Entity;
-                var now = DateTime.UtcNow;
-
-                if (entry.State != EntityState.Deleted)
-                {
-                    if (entry.State == EntityState.Added)
-                    {
-                        entity.CreatedOn = now;
-                        entity.CreatedBy = "calicubilly";
-                    }
-                    else
-                    {
-                        base.Entry(entity).Property(x => x.CreatedBy).IsModified = false;
-                        base.Entry(entity).Property(x => x.CreatedOn).IsModified = false;
-                    }
-
-                    entity.UpdatedOn = now;
-                    entity.UpdatedBy = "calicubilly";
-                }
-                else
-                {
-                    // if deleted state, change to Modified
-                    entry.State = EntityState.Modified;
-                    var entityDel = (ISoftDeletableEntity)entry.Entity;
-                    entityDel.IsDeleted = 1;
-                    entityDel.DeletedOn = now;
-                    entityDel.DeletedBy = "calicubilly";
-                }
-            }
-        }
+       
     }
 }
