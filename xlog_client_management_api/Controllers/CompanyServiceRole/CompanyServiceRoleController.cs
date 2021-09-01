d3eb94f413369d87c1cbc6b939b7d6b3e8609a7e
jerson <jerson.baltazar@myxlog.com>
2021-09-01 09:56:24 +0800

    Added export for csv and excel for company service role (ACM Groups)

--- xlog_client_management_api/Controllers/CompanyServiceRole/CompanyServiceRoleController.cs
+++ xlog_client_management_api/Controllers/CompanyServiceRole/CompanyServiceRoleController.cs
@@ -73,12 +73,11 @@ namespace xlog_client_management_api.Controllers.CompanyServiceRole
         [ProducesResponseType(StatusCodes.Status200OK)]
         [ProducesResponseType(StatusCodes.Status400BadRequest)]
         [ProducesResponseType(StatusCodes.Status401Unauthorized)]
-        public async Task<IActionResult> DownloadCompanyServiceByCompanyId(string companyId)
+        public async Task<IActionResult> DownloadCompanyServiceByCompanyId(string companyId,[FromQuery] string type = "xlsx")
         {
-            var response = await _companyServiceRole.DownloadByCompanyServiceId(companyId);
-            var fileName = $"UserGroups_{DateTime.Now:yyyyMMddhhmmss}.xlsx";
+            var response = await _companyServiceRole.DownloadByCompanyServiceId(companyId, type);
 
-            return File(response, "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet", fileName);
+            return File(response.Bytes, MimeTypes.GetMimeType(response.FileName), response.FileName);
         }
 
         [Route("company/services/role/company/{companyId}")]
